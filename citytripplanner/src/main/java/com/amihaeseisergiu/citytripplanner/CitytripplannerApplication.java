83a87d7a41c786860ea1a978874e3df6ac9b56ec
Amihaeseisergiu <61450080+Amihaeseisergiu@users.noreply.github.com>
2021-02-22 21:42:04 +0200

    Builded project
    Installed necessary dependencies for integrating tailwind and thymeleaf with spring boot

---
+++ citytripplanner/src/main/java/com/amihaeseisergiu/citytripplanner/CitytripplannerApplication.java
@@ -0,0 +1,13 @@
+package com.amihaeseisergiu.citytripplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitytripplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitytripplannerApplication.class, args);
+	}
+
+}
---
+++ citytripplanner/src/test/java/com/amihaeseisergiu/citytripplanner/CitytripplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amihaeseisergiu.citytripplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitytripplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
